4008a004a8d7fb8a65747b19fae83d68a8169e3e
artem2284708 <artemmmmkk@gmail.com>
2023-10-27 13:19:22 +0300

    update training.py

--- training.py
+++ training.py
@@ -4,3 +4,10 @@ text = '[\\xd0\\x9f\\xd1\\x80\\xd0\\xb8\\xd0\\xbf\\xd0\\xb5\\xd0\\xb2: KDDK & Fe
 decoded_text = codecs.escape_decode(text.encode('latin-1'))[0].decode('utf-8')
 print(decoded_text)
 
+"""string = '[\\xd0\\x9f\\xd1\\x80\\xd0\\xb8\\xd0\\xbf\\xd0\\xb5\\xd0\\xb2: KDDK & Fetty Wap]'
+
+string = string.encode().decode('utf-8')
+res = "b'" + string + "'"
+print(res)
+dec = res.encode().decode('utf-8')
+print(dec)"""
